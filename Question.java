1c3cd0957dd3dce4cc82eaa256171216a01262af
mehraritvik12 <112796687+mehraritvik12@users.noreply.github.com>
2023-10-12 22:23:33 +0530

    Rain_Water_Quetion

---
+++ Question.java
@@ -0,0 +1,31 @@
+//package RainWaterQuestion;
+//
+//public class Question {
+////	
+//	public static void (int number[]) {
+//		
+//		//leftMax
+//		 int leftMax[]=new int[height.length];
+//		leftMax[0] = height[0];
+//	for(int i = 1;i<height.length;i++) {
+//			leftMax[i]=Math.max(height[i],leftMax[i-1]);
+//		}
+//		
+//		
+//		//RightMax
+//		//water level
+//	    //trappedwater
+//		
+//	}
+//
+//	
+	
+	
+	
+
+//	public static void main(String[] args) {
+//
+//
+//	}
+//
+//}
