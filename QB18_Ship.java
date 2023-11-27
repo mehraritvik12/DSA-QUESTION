ff7fe27f0f98bd47bb1439ebefb0536cef29d43c
mehraritvik12 <112796687+mehraritvik12@users.noreply.github.com>
2023-11-28 02:15:25 +0530

    Add files via upload

---
+++ QB18_Ship.java
@@ -0,0 +1,70 @@
+package Searching;
+
+import java.util.ArrayList;
+
+public class QB18_Ship {
+
+    public static int leastWeightCapacity(int[] weights, int d) {
+//        int start = 1;
+//        int sum = 0;
+//        for (int i = 0; i < weights.length; i++) {
+//            sum = sum + weights[i];
+//        }
+//        int end = sum;
+
+        int start = Integer.MIN_VALUE, end = 0;
+        for (int i = 0; i < weights.length; i++) {
+            end += weights[i];
+            start = Math.max(start, weights[i]);
+        }
+
+
+        while (start <= end) {
+        int mid = start +(end-start)/2;
+
+
+           if(givemin(weights, mid)<=d){
+               end=mid-1;
+
+           }
+           else {
+               start = mid+1;
+           }
+        }
+        return start;
+
+    }
+
+    private static int givemin(int[] arr, int cap) {
+        int n = arr.length;
+        int sum =0;
+        int count=1 ;
+        ArrayList<Integer> list= new ArrayList<>();
+
+    for(int i=0;i<n;i++){
+        if(sum+arr[i]<=cap){
+            sum+=arr[i];
+        }
+        else{
+            list.add(sum);
+            sum=arr[i];
+            count++;
+        }
+
+    }
+
+        return count;
+
+
+
+    }
+
+    public static void main(String[] args) {
+        int arr[]= {5,4,5,2,3,4,5,6};
+        int d = 5;
+        //System.out.println(givemin(arr,cap));
+        System.out.println( leastWeightCapacity(arr,d));
+
+    }
+
+}
